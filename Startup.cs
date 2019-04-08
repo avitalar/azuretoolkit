1f7cd9aa32bafb1bd758f2745a3120183435bdc2
avitalar <avitalar1@gmail.com>
2019-04-08 11:47:44 +0300

    av

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+
 namespace AzureToolkit2
 {
     public class Startup
