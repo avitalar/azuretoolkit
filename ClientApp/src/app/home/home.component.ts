134c0e57883f5666462bff113127b5694881025b
avitalar <avitalar1@gmail.com>
2019-04-16 14:55:32 +0300

    user service

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../common/services/user.service';
+import { User } from '../common/models/user';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  user: User;
+  ngOnInit(): void {
+    this.userService.getUser().subscribe(user => this.user = user );
+  }
+
+  constructor(private userService: UserService) { }
+  
 }
