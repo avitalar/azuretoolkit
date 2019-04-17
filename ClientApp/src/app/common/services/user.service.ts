306bc0f353c7efdc2214483b34cf04c1909c874d
avitalar <avitalar1@gmail.com>
2019-04-17 20:42:08 +0300

    jobTitle

--- ClientApp/src/app/common/services/user.service.ts
+++ ClientApp/src/app/common/services/user.service.ts
@@ -31,7 +31,7 @@ export class UserService {
                             case 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname':
                                 user.lastName = claim.val;
                                 break;
-                              case 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/jobtitle':
+                              case 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/jobTitle':
                                 user.jobtitle = claim.val;
                                 break;
                         }
